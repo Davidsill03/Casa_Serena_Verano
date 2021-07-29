60eea4c10d08c3895fe50fe0847332e23c62c63c
david <david.vaz.sig@gmail.com>
2021-07-29 13:08:10 -0500

    Button FB, LLamar y ubicación Imagenes prueba nube

--- app/src/main/java/com/example/myapplication/FisicasActivity.java
+++ app/src/main/java/com/example/myapplication/FisicasActivity.java
@@ -11,14 +11,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class FisicasActivity extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fisicas);
 
+        imageView = findViewById(R.id.image1fisicas);
+
+        String url = "https://www.dropbox.com/s/lbxxi7b3viwkwks/actividad_fisica.jpg?dl=0";
+        Glide.with(this)
+                .load(url)
+                .placeholder(R.drawable.logorosa)
+                .error(R.drawable.logorosa)
+                .into(imageView);
     }
     public void Configg(View view) {
         Intent sigueinte = new Intent(this, ConfiguracionActivity.class);
